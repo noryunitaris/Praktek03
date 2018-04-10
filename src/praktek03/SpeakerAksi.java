d44e77eac8eb57f85ceeee375414559992265bba
noryunitaris <yunitarisnaida@gmail.com>
2018-04-10 12:07:31 +0700

    Instance buat atribut dan panggil method

--- src/praktek03/SpeakerAksi.java
+++ src/praktek03/SpeakerAksi.java
@@ -1,5 +1,12 @@
 package praktek03;
 public class SpeakerAksi {
-    
+    public static void main(String[] args) {
+        Speaker s = new Speaker();
+        s.merk="Bowers&Wilkins";
+        s.jenis="Audio";
+        s.harga=2500000;
+        
+        s.informasi();
+    }
     
 }
