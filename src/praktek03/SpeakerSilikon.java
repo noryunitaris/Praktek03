4baa173aaebfc66dd819fffe5ef3e76de05a72e0
noryunitaris <yunitarisnaida@gmail.com>
2018-04-10 12:34:28 +0700

    getter and setter

--- src/praktek03/SpeakerSilikon.java
+++ src/praktek03/SpeakerSilikon.java
@@ -1,5 +1,19 @@
 package praktek03;
 
 public class SpeakerSilikon extends Speaker{
+    private String jenisSpeaker;
+    
+    void throttle(){
+        System.out.println("--Audio--");
+    }
+
+    public String getJenisSpeaker() {
+        return jenisSpeaker;
+    }
+
+    public void setJenisSpeaker(String jenisSpeaker) {
+        this.jenisSpeaker = jenisSpeaker;
+    }
+    
     
 }
