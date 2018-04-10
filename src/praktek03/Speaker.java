d24f522ded4ffac897ce21068589566ebbdf9eff
noryunitaris <yunitarisnaida@gmail.com>
2018-04-10 11:56:41 +0700

    Membuat Atribut dan method informasi

---
+++ src/praktek03/Speaker.java
@@ -0,0 +1,14 @@
+package praktek03;
+
+public class Speaker {
+    String merk;
+    String jenis;
+    double harga;
+    
+    void informasi(){
+        System.out.println("Merk \t: "+merk+"\n"+
+                "Jenis \t: "+jenis+"\n"+
+                "Harga \t:"+harga);
+    }
+    
+}
