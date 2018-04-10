c6adec3935902d8de0057b50de2c4c789f27de7d
noryunitaris <yunitarisnaida@gmail.com>
2018-04-10 12:26:48 +0700

    menggunakan method get

--- src/praktek03/SpeakerAksi.java
+++ src/praktek03/SpeakerAksi.java
@@ -2,11 +2,19 @@ package praktek03;
 public class SpeakerAksi {
     public static void main(String[] args) {
         Speaker s = new Speaker();
-        s.merk="Bowers&Wilkins";
-        s.jenis="Audio";
-        s.harga=2500000;
+   
+        s.setMerk("Bower&wilkins");
+        s.setJenis("Audio");
+        s.setHarga(2500000);
         
         s.informasi();
+        
+        System.out.print("Merknya \t: ");
+        System.out.println(s.getMerk());
+        System.out.print("Jenisnya \t: ");
+        System.out.println(s.getJenis());
+        System.out.print("Harganya \t: ");
+        System.out.println(s.getHarga());
     }
     
 }
