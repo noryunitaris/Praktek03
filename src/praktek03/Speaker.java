c7a9fc24515c84e443b5d99ef8c069a71df2203f
noryunitaris <yunitarisnaida@gmail.com>
2018-04-10 12:11:11 +0700

    buat getter dan setter

--- src/praktek03/Speaker.java
+++ src/praktek03/Speaker.java
@@ -1,14 +1,39 @@
 package praktek03;
 
 public class Speaker {
-    String merk;
-    String jenis;
-    double harga;
+    private String merk;
+    private String jenis;
+    private double harga;
     
     void informasi(){
         System.out.println("Merk \t: "+merk+"\n"+
                 "Jenis \t: "+jenis+"\n"+
                 "Harga \t:"+harga);
     }
+
+    public String getMerk() {
+        return merk;
+    }
+
+    public void setMerk(String merk) {
+        this.merk = merk;
+    }
+
+    public String getJenis() {
+        return jenis;
+    }
+
+    public void setJenis(String jenis) {
+        this.jenis = jenis;
+    }
+
+    public double getHarga() {
+        return harga;
+    }
+
+    public void setHarga(double harga) {
+        this.harga = harga;
+    }
+    
     
 }
